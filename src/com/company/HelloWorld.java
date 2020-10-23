9bd55c6bd741d52587ee843f5c427793e7fe0775
kmtso <mingtso@outlook.com>
2020-10-23 15:24:00 +0800

    HelloWorld

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld. It is Ming");
+        System.out.println("It is Develop by Tso Kei Ming, OOP, 2020");
+    }
+}
